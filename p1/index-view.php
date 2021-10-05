ed6d11dedc4022d49a17ebf4540285a0eda5b6a5
Ana Tolentino Cruz <ant513@g.harvard.edu>
2021-10-05 18:11:43 +0000

    Adjusted View and Css

--- p1/index-view.php
+++ p1/index-view.php
@@ -13,18 +13,48 @@
 
     <h2>Mechanics</h2>
     <ul>
-        <li></li>
+        <li>Each play will cost the player $2</li>
+        <li>Choosing Numbers - The player will have the option to either:
+            <ul>
+                <li>Select five numbers in acending order from 1 to 69 for the white balls
+                    and one number from 1 to 26 for the red ball, the PowerBall. (For this version, this is option is
+                    not
+                    included). </li>
+                <li>Quickpick - Have random numbers picked for the player.</li>
+            </ul>
+        </li>
+        <li>The computer will randomly generate 5 numbers from 1 to 69 for the white balls, and 1 for PowerBall from 1
+            to 26 for the drawing.</li>
+        <li>If the player's ball numbers does not match any of the drawn ball numbers, the player does not win.</li>
+        <li><b>Winning Prizes:</b> Depending on the number of matches the player may receive a prize.
+            <ul>
+                <li>If the player matches all 5 white ball numbers and the PowerBall to the ball numbers drawn, the
+                    player wins
+                    the Jackpot!</li>
+                <li>If the player only matches the 5 white ball numbers to the ball numbers drawn, the player wins the
+                    second prize
+                    of $1,000,000.</li>
+                <li>If the player matches 4 white ball numbers and the PowerBall, the player wins the third prize of
+                    $50,000.</li>
+                <li>If the player only matches 4 white ball numbers, the player wins the fourth prize of $100.</li>
+                <li>If the player matches 3 white ball numbers and the PowerBall, the player wins the fifth prize of
+                    $100.</li>
+                <li>If the player only matches 3 white ball numbers, the player wins the sixth prize of $7.</li>
+                <li>If the player matches 2 white ball numbers and the PowerBall, the player wins the seventh prize of
+                    $7.</li>
+                <li>If the player matches 1 white ball number and the PowerBall, the player wins the eighth prize of $4.
+                </li>
+                <li>If the play only matches the PowerBall, the player wins the ninth prize of $4.</li>
+                <li><u>Note:</u> If the player matches 2 white ball numbers or 1 white ball number, the player does not
+                    win a prize. </li>
+            </ul>
+        </li>
     </ul>
 
     <h2>Results</h2>
-    <ul>
-        <li>...</li>
-    </ul>
-
-    <main>
-        <h1>Powerball</h1>
-        <div>
-            <h2>Powerball Results: </h2>
+    <ul class="results">
+        <li>
+            <h3>Powerball Results: </h3>
             <span class="balls">
                 <?php echo $ball1 ?>
             </span>
@@ -43,11 +73,9 @@
             <span class="powerball">
                 <?php echo $powerball?>
             </span>
-        </div>
-
-
-        <div>
-            <h2>Player's Numbers</h2>
+        </li>
+        <li>
+            <h3>Player's Numbers: Quickpick</h3>
 
             <span class="balls">
                 <?php echo $player_ball1 ?>
@@ -67,16 +95,18 @@
             <span class="powerball">
                 <?php echo $player_powerball?>
             </span>
-        </div>
-        <div>
-            <h2>Matches: <?php echo $matches  ?></h2>
+        </li>
+        <li>
+            <h3>Matches: <?php echo $matches  ?></h3>
             <?php echo(thisFunction($playerMatches) . $powerballMatch);?>
-        </div>
-        <div>
-            <h2>Winings: <?php echo $winnings ?></h2>
-            <h2>Player Spent: $<?php echo $cost;?></h2>
-        </div>
-    </main>
+        </li>
+        <li>
+            <h3>Winings: <?php echo $winnings ?></h3>
+        </li>
+        <li>
+            <h3>Player Spent: $<?php echo $cost;?></h3>
+        </li>
+    </ul>
 
 </body>
 
