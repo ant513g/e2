11f27bcec6a770ea0c1cb8219cd8176798f330ac
Ana Tolentino Cruz <ant513@g.harvard.edu>
2021-11-03 01:25:16 +0000

    Update

--- rps/app/MyGame.php
+++ rps/app/MyGame.php
@@ -1,18 +1,18 @@
 <?php
 
-class MyGame extends Game
-{
-    protected $moves = ['heads', 'tails'];
+// class MyGame extends Game
+// {
+//     protected $moves = ['heads', 'tails'];
 
-    protected function determineOutcome($playerMove, $computerMove)
-    {
-        if ($computerMove == $playerMove) {
-            return 'tie';
-        } elseif ($playerMove == 'tails' and $computerMove == 'heads' or $playerMove == 'paper' and $computerMove == 'rock' or $playerMove == 'scissors' and $computerMove == 'paper') {
-            return 'won';
-        } else {
-            return 'lost';
-        }
-    }
-}
+//     protected function determineOutcome($playerMove, $computerMove)
+//     {
+//         if ($computerMove == $playerMove) {
+//             return 'tie';
+//         } elseif ($playerMove == 'tails' and $computerMove == 'heads' or $playerMove == 'paper' and $computerMove == 'rock' or $playerMove == 'scissors' and $computerMove == 'paper') {
+//             return 'won';
+//         } else {
+//             return 'lost';
+//         }
+//     }
+// }
     
