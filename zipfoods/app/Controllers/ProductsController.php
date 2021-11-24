4131ce9f714d0b550f9a58329ffc9c454c3d4a53
Ana Tolentino Cruz <ant513@g.harvard.edu>
2021-11-24 18:09:16 +0000

    Class commit

--- zipfoods/app/Controllers/ProductsController.php
+++ zipfoods/app/Controllers/ProductsController.php
@@ -25,18 +25,52 @@ class ProductsController extends Controller
     {
         $sku = $this->app->param('sku');
 
+        if(is_null($sku)) {
+            $this->app->redirect('/products');
+        }
+
+
         $product = $this->productsObj->getBySku($sku);
 
+
+        
         // if(is_null($product)) {
         //     return $this->app->view('errors/404');
         // }
         if(is_null($product)) {
             return $this->app->view('products/missing');
         }
+
+        $reviewSaved = $this->app->old('reviewSaved');
+
+        
         // if(is_null($sku)) {
         //     return $this->app->view('products/missing');
         // }
 
-        return $this->app->view('products/show', ['product' => $product]);
+        return $this->app->view('products/show', [
+            'product' => $product,
+            'reviewSaved' => $reviewSaved
+        ]);
+    }
+    public function saveReview() 
+    {
+        $this->app->validate([
+            'sku' => 'required', 
+            'name' => 'required',
+            'review' => 'required|minLength:200'
+           
+        ]);
+        # If the above validation checks fail
+        # The user is required back to where they came from (/product)
+        # None of the code that follows will be executed
+        
+        return $this->app->view('example');
+       $sku = dump($this->app->input('sku'));
+       $name = dump($this->app->input('name'));
+       $review = dump($this->app->input('review'));
+
+       #Todo: Persist review to the database
+       return $this->app->redirect('/product?sku=' . $sku, ['reviewSaved' => true ]);
     }
 }
